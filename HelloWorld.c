29887c831826acc801d16ba36fb425bbfc062070
luissshm <luish.99@hotmail.com>
2022-04-09 13:47:02 -0400

    Second Commit

---
+++ HelloWorld.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+
+  char a = 'H', b = 'e', c = 'l';
+  char d = 'o', e = 'W', f = 'r',  g = 'd';
+
+  printf("%c%c%c%c%c %c%c%c%c%c", a,b,c,c,d,e,d,f,c,g);
+
+  return 0;
+}
+
+
+
+
